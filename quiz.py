d10386da7969415c15077a1b059cf2700bb3e144
KADEEJA SAHLA <99816026+KadeejaSahlaPV@users.noreply.github.com>
2023-12-28 21:16:41 +0530

    State and Capital

---
+++ quiz.py
@@ -0,0 +1,74 @@
+import random
+
+states_and_capitals = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Moines',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Minnesota': 'St. Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City',
+    'New Hampshire: 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakota': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne',
+}
+
+def state_capital_quiz():
+    print("Welcome to the State and Capital Quiz!")
+
+    states = list(states_and_capitals.keys())
+
+    random.shuffle(states)
+
+    for state in states:
+        correct_capital = states_and_capitals[state]
+
+        user_answer = input(f"What is the capital of {state}? ").strip()
+
+        
+        if user_answer.lower() == correct_capital.lower():
+            print("Correct!\n")
+        else:
+            print(f"Wrong! The capital of {state} is {correct_capital}.\n")
+
+    print("Quiz completed. Good job!")
+
+state_capital_quiz()
